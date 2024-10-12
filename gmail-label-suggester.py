daa21d02336b3b05fe3e8ad5c392a0dbfbe77b28
bcvilnrotter <bcvilnrotter@gmail.com>
2024-10-12 17:12:49 -0400

    ready for first PR

--- gmail-label-suggester.py
+++ gmail-label-suggester.py
@@ -5,21 +5,23 @@ from googleapiclient.discovery import build
 import sys,argparse,os,base64,re,itertools
 from datetime import datetime,timezone
 from collections import Counter
+from collections import defaultdict
 from lxml import html
 from nltk import ngrams
 import nltk
 
 # %%
 # download the specific packages for helping navigate through the n-gram part of the analysis
-nltk.download('punkt')
+nltk.download('punkt',download_dir=os.getcwd())
+nltk.download('averaged_perceptron_tagger',download_dir=os.getcwd())
+nltk.download('averaged_perceptron_tagger_eng',download_dir=os.getcwd())
 
 # Define the scopes that will be used when interacting with your gmail account
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.readonly',
     'https://www.googleapis.com/auth/gmail.settings.basic'
 ]
-
-# %%
+# %% - do not run
 # region argparse
 # Prepare the argparse section for the script to function
 parser = argparse.ArgumentParser()
@@ -81,6 +83,7 @@ def authenticate_gmail(creds_file):
     flow = InstalledAppFlow.from_client_secrets_file(creds_file,SCOPES)
     creds = flow.run_local_server(port=0)
     service = build('gmail','v1',credentials=creds)
+    log('- service authenticated.')
     return service
 
 # function to get the labelId for the label name provided by the user. This function should be used in conjunction with get_emails, but can be used elsewhere
@@ -106,19 +109,11 @@ def clean_html(raw_html):
     clean_text=tree.text_content().strip()
     clean_text=re.sub(r'\s+',' ',clean_text)
     return clean_text
-"""
-def clean_html(raw_html):
-    clean_text = re.sub(r'<[^>]+>','',raw_html)
-    clean_text = unescape(clean_text)
-    clean_text = re.sub(r'\s+',' ',clean_text).strip()
-    return clean_text
-"""
 
 # function to collect the emails identified by the user (based on email used to log in using credentials and label string)
 def get_emails(service,label):
 
-    # iterate through the results and clean the body of the messages and return a giant string containt all the body data
-    # need to add pagination
+    # initialize local variables
     email_contents=[]
     page_token = None
     total_messages = 0
@@ -159,45 +154,114 @@ def get_emails(service,label):
 
     return email_contents
 
+# function to identify label IDs then collect email content based on label_id
+def pull_emails_by_label(service,labels):
+
+    emails_by_label = {}
+
+    for label,label_id in identify_label_ids(service,labels).items():
+        email_content = get_emails(service,label_id)
+        emails_by_label[label] = email_content
+    
+    return emails_by_label
+
 # function to analyze a large string of information to pull out the most common phrase using n-grams
 # n is the number of words to compare sequentially in a string
 # n_common is the number of most common phraases found
 def analyze_email_content(email_content,n=4,n_common=10):
+    
+    # initialize and manipulate local variables
     email_content = re.sub(r'\s+',' ',email_content).strip() # remove excess whitespace
     words = [word for word in re.findall(r'\b\w{3,}\b', email_content.lower())]
     n_grams = ngrams(words,n)
     common_phrases = Counter(n_grams).most_common(n_common)
+
     return common_phrases
 
-#endregion
-# %%
-# main function initiation and call
-def main():
-    
-    # authenticate the session using the credentials provided by the project
-    service = authenticate_gmail(args.credentials)
-    log('- service authenticated.')
+# create and build out a dictionary to contain all the label name:id pairs requested for iteration
+def identify_label_ids(service,labels):
 
-    # identify the number of labels provided, and then iterate through them
-    log(f'- {len(args.labels)} identified label name to run analysis on.')
-    # create and build out a dictionary to contain all the label name:id pairs requested for iteration
+    # initialize local varaible(s)
     label_dict = {}
-    for label in list(args.labels):
+
+    for label in labels:
         label_dict.update({label:get_label_id(service,label)})
     log(f'- converted provided label names to a dict of label name and id pairs')
 
-    for label in label_dict:
+    return label_dict
+
+# function used to collect all common phrases generated from emails of 
+def process_common_phrases(email_content,n=4,n_common=10):
+    
+    # initialize local variables
+    label_phrases = defaultdict(list)
+    
+    for label,email_content in email_content.items():        
         log(f'|- working on [{label}] label.')
-        email_contents = get_emails(service,label_dict[label])
-        log(f'|- content from {len(email_contents)} messages were pulled.')
+        
         # convert list of email contents into a giant string for easier n-gram analysis
-        common_phrases = analyze_email_content(' '.join(email_contents),n=args.num_phrase_compare_window,n_common=args.num_common_phrases_pulled)
-        # create a condition string that can be used in a filter
-        #filter_query = ' OR '.join(['"{} {}"'.format(phrase[0][0],phrase[0][1]) for phrase in common_phrases[:args.num_common_phrases]])
-        #TODO Need to 
-        log(f'|- filter query created:')
-        log(f'   |- [{common_phrases}]')
-        #create_gmail_filter
+        label_phrases[label] = set(analyze_email_content(' '.join(email_content),n,n_common))
+    
+    return label_phrases
+
+def compare_common_phrases(label_phrases):
+
+    # initialize local variables
+    unique_phrases = {}
+
+    for label, phrases in label_phrases.items():
+        all_other_phrases = set().union(*[set(p[0]) for l,p in label_phrases.items() if l != label])
+        unique_to_label = [(p,c,u) for p,c,u in phrases if not set(p).intersection(all_other_phrases)]
+        unique_phrases[label] = unique_to_label
+    
+    return unique_phrases
+
+def construct_gmail_filter_queries(phrases,num_words=4,top_n=3):
+
+    for label,phrases in phrases.items():
+        filter_query = []
+        #sorted_phrases = sorted(phrases,key=lambda x: x[1], reverse=True)[:top_n]
+        for phrase,_,_ in phrases[:top_n]:
+            words_to_use = min(num_words,len(phrase))
+            phrase = ' '.join(phrase[:words_to_use])
+            filter_query.append(f'"{phrase}"')
+        log(f'|- filter query created for label [{label}]:')
+        filter_string = ' OR '.join(filter_query)
+        log(f'   |- [{filter_string}]')
+
+def generate_global_ngram_frequency(common_phrases,n=4):
+    ngram_counts = Counter()
+    for phrases in common_phrases.values():
+        for phrase,_ in phrases:
+            ngram_counts.update(ngrams(phrase,n))
+    return ngram_counts
+
+def generate_phrase_uniqueness(phrases,global_ngram_counts,n=4):
+    unique_phrases=[]
+    for phrase,count in phrases:
+        uniqueness_score = sum(1/global_ngram_counts[ngram] for ngram in ngrams(phrase,n))
+        unique_phrases.append((phrase,count,uniqueness_score))
+    return sorted(unique_phrases,key=lambda x: x[2],reverse=True)
+
+def generate_unique_phrases(emails_by_label,n=4,n_common=10):
+    label_phrases = {label: analyze_email_content(' '.join(emails),n=n,n_common=n_common) for label,emails in emails_by_label.items()}
+    global_ngram_counts = generate_global_ngram_frequency(label_phrases,n)
+    return {label: generate_phrase_uniqueness(phrases,global_ngram_counts,n)[:n_common] for label,phrases in label_phrases.items()}
+#endregion
+# %%
+
+# main function initiation and call
+def main():
+    creds = args.credentials
+    labels = args.labels
+    
+    # authenticate the session using the credentials provided by the project
+    service = authenticate_gmail(creds)
+    # identify the number of labels provided, and then iterate through them
+    log(f'- {len(args.labels)} identified label name to run analysis on.')
+    # create the omnibus embedded function loop that will be factored out as more options and settings are added
+    construct_gmail_filter_queries(compare_common_phrases(generate_unique_phrases(pull_emails_by_label(service,identify_label_ids(service,list(labels))))))
+    #TODO create_gmail_filter
 
 if __name__ == "__main__":
     main()
